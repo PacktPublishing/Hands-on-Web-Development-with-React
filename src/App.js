df52fb622a36a4d9a592e12c55e20cd5c9027a1a
Filip Danić <filipdanic7@gmail.com>
2018-05-13 20:47:51 +0200

    add code for section 1 video “React, JSX, and Your First Component”

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Counter from './components/Counter';
 
 class App extends Component {
   render() {
@@ -10,6 +11,9 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
+        <Counter />
+        <Counter />
+        <Counter />
       </div>
     );
   }
---
+++ src/components/Counter.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import './Counter.css';
+
+export default class Counter extends React.Component {
+  state = {
+    counter: 0,
+  };
+
+  increment = () => {
+    this.setState({
+      counter: this.state.counter + 1,
+    });
+  };
+
+  decrement = () => {
+    this.setState({
+      counter: this.state.counter - 1,
+    });
+  };
+
+  render() {
+    const { counter } = this.state;
+    return(
+      <div className="counter-box">
+        <button
+          onClick={this.increment}
+        >
+          +1
+        </button>
+        <span className={counter < 0 ? 'counter-box--danger' : ''}>
+          {counter}
+        </span>
+        <button
+          onClick={this.decrement}
+        >
+          -1
+        </button>
+      </div>
+    )
+  }
+}
