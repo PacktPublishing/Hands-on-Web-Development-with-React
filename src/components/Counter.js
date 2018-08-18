aa0720d248db459bb760b00c1b2b5cdf80f98e21
Filip Danić <filip@spicefactory.co>
2018-08-18 19:24:57 +0200

    add code for section 7 video “It‘s just functions”

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,42 +1,50 @@
 import React from 'react';
 import './Counter.css';
 
+const button = (onClick, label) =>
+  React.createElement(
+    'button',
+    { onClick },
+    `${label}`
+  );
+
+const counterText = (label, count) =>
+  React.createElement(
+    'span',
+    { className: count < 0 ? 'counter-box--danger' : '' },
+    `${label}`
+  );
+
+const addTwo = (a, b) => a + b;
+
+const increment = (amount) => (state) => ({
+  counter: addTwo(state.counter, amount),
+});
+
 export default class Counter extends React.Component {
   state = {
     counter: 0,
   };
 
-  increment = () => {
-    this.setState({
-      counter: this.state.counter + 1,
-    });
-  };
+  increment = () =>
+    this.setState(increment(1));
 
-  decrement = () => {
-    this.setState({
-      counter: this.state.counter - 1,
-    });
-  };
+  decrement = () =>
+    this.setState(increment(-1));
 
   render() {
     const { counter } = this.state;
     const { name } = this.props;
     return(
-      <div className="counter-box">
-        <button
-          onClick={this.increment}
-        >
-          +1
-        </button>
-        <span className={counter < 0 ? 'counter-box--danger' : ''}>
-          {name} {counter}
-        </span>
-        <button
-          onClick={this.decrement}
-        >
-          -1
-        </button>
-      </div>
+      React.createElement(
+        'div',
+        { className: 'counter-box'},
+        [
+          button(this.increment, '+1'),
+          counterText(`${name} ${counter}`, counter),
+          button(this.decrement, '-1')
+        ]
+      )
     )
   }
 }
